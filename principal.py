557b2788eeb76772110b032dcd22e45f19d96a2d
Camilo Andres Hernandez Gutierrez <hgcamilo94@gmail.com>
2026-03-14 21:22:05 -0500

    Secreo la funcion modulo de registro

--- principal.py
+++ principal.py
@@ -12,7 +12,7 @@ def menu():                             #se crea el menu
             """)
             opcion = int(input("Seleccione una opcion: "))
             if opcion == 1:
-                print("ANDRES")
+                registrar_gasto()
                 break
             elif opcion == 2:
                     print("YEISON")
@@ -23,8 +23,28 @@ def menu():                             #se crea el menu
             else:
                 print("Opcion invalida")
 
+#Camilo Andres Hernandez (Modulo de registro)
 
+def registrar_gasto():
+
+    placa = input("Ingrese la placa del vehículo: ")
+    concepto = input("Ingrese el concepto (Gasolina o Peaje): ")
+    valor = float(input("Ingrese el valor del gasto: "))
+
+    flujo = {
+     "placa": placa,
+     "concepto": concepto,
+     "valor": valor
+    }
+
+    gastos.append(flujo)
+
+    print("Gasto registrado correctamente.")
+    print(gastos)
 
 if __name__ == "__main__":
     menu()
+
+
+
 
